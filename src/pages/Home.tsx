6abca8173e6e2c03ced79f7d3e395fc84e898b9c
ahaddad98 <amine.haddadd@gmail.com>
2022-02-27 02:35:09 +0100

    nav bar not complete

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,11 +8,12 @@ import {ReactComponent as Background} from "../assets/background.svg"
 //@ts-ignore
 import {ReactComponent as Logo} from "../assets/logo.svg"
 // import button_singin from "../assets/button_singin.svg"
-
+import { NavLink } from "react-router-dom";
 const Home = () => {
     return (
         <div className="home">
-            <Background className="bg" />
+            <Background className="bg">
+            </Background>
             <div className="background">
                 <div className="Section1">
                 <Logo className="logo" />
@@ -20,6 +21,27 @@ const Home = () => {
                     <p className="parag">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, corporis. Magni enim sit eos quia ad dicta beatae sint, esse modi at unde asperiores dolor eaque laudantium, voluptates provident suscipit.
                     </p>
                     <button className="button">SIGN IN</button>
+                </div>
+                <div className="navigation">
+                <NavLink className={(navData) => navData.isActive ? "nav-active" : "" }  to="/" 
+                    >
+                    Home
+                </NavLink>
+                <NavLink className={(navData) => navData.isActive ? "nav-active" : "" }  to="/" 
+                    >
+                    About
+                </NavLink>
+                <NavLink className={(navData) => navData.isActive ? "nav-active" : "" }  to="/" 
+                    >
+                    Learning
+                </NavLink>
+                <NavLink className={(navData) => navData.isActive ? "nav-active" : "" }  to="/" 
+                    >
+                    Servises
+                </NavLink>
+                    {/* <section className="home-button">
+                        Home
+                    </section> */}
                 </div>
                 <div className="Section2">
                 <img src={player} />
