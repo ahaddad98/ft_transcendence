931041e568e3d4f7d195311b172096bcabd9b189
ahaddad98 <amine.haddadd@gmail.com>
2022-02-27 21:55:23 +0100

    landing page responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,74 +1,93 @@
 import React from "react";
 // import Background from "../components/Background";
-import "../styles/components/style.css"
+import "../styles/components/style.css";
 //@ts-ignore
-import player from "../assets/player.svg"
+import player from "../assets/player.svg";
 //@ts-ignore
-import {ReactComponent as Background} from "../assets/background.svg"
+import { ReactComponent as Background } from "../assets/background.svg";
 //@ts-ignore
-import {ReactComponent as Logo} from "../assets/logo.svg"
+import { ReactComponent as Logo } from "../assets/logo.svg";
 // import button_singin from "../assets/button_singin.svg"
 import { NavLink } from "react-router-dom";
+import MediaQuery from "react-responsive";
+// import { MediaQuery } from 'react-responsive'
+
 const Home = () => {
-    return (
-        <div className="home">
-            <Background className="bg">
-            </Background>
-            <div className="background">
-                <div className="Section1">
-                <Logo className="logo" />
-                    <h1 className="title">PING PONG GAME</h1>
-                    <p className="parag">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, corporis. Magni enim sit eos quia ad dicta beatae sint, esse modi at unde asperiores dolor eaque laudantium, voluptates provident suscipit.
-                    </p>
-                    <button className="button">SIGN IN</button>
-                </div>
-                <div className="navigation">
-                    <div className="nav-active">
-
-                    <NavLink className={(navData) => navData.isActive ? "nave-active" : "" }  to="/" 
-                    >
-                        Home
-                    </NavLink>
-                        </div>
-                        <div className="nav-active">
-
-                    <NavLink className={(navData) => navData.isActive ? "nave-active" : "" }  to="/" 
-                    >
-                        About us
-                    </NavLink>
-                        </div>
-                        <div className="nav-active">
-
-                    <NavLink className={(navData) => navData.isActive ? "nave-active" : "" }  to="/" 
-                    >
-                        Learning
-                    </NavLink>
-                        </div>
-                        <div className="nav-active">
-
-                    <NavLink className={(navData) => navData.isActive ? "nave-active" : "" }  to="/" 
-                    >
-                        Services
-                    </NavLink>
-                        </div>
-                {/* <NavLink className={(navData) => navData.isActive ? "nav-active" : "" }  to="/" 
-                    >
-                    About
-                </NavLink>
-                <NavLink className={(navData) => navData.isActive ? "nav-active" : "" }  to="/" 
-                    >
-                    Learning
-                </NavLink>
-                <NavLink className={(navData) => navData.isActive ? "nav-active" : "" }  to="/" 
-                    >
-                    Servises
-                </NavLink> */}
-                </div>
-                <div className="Section2">
-                <img src={player} />
-                </div>
+  return (
+    <div className="home">
+      <MediaQuery minWidth={1121}>
+        <Background className="bg"></Background>
+        <div className="background">
+          <div className="Section1">
+            <Logo className="logo" />
+            <h1 className="title">PING PONG GAME</h1>
+            <div className="paragresp">
+              <p style={{ textAlign: "justify", lineHeight: 1.6 }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+                corporis. Magni enim sit eos quia ad dicta beatae sint, esse
+                modi at unde asperiores dolor eaque laudantium, voluptates
+                provident suscipit.
+              </p>
             </div>
+            <div className="button">
+                <div className="butt-des">SIGN IN</div>
+            </div>
+          </div>
+          <div className="navigation">
+            <div className="nav-active">
+              <NavLink
+                className={(navData) => (navData.isActive ? "nave-active" : "")}
+                to="/"
+              >
+                Home
+              </NavLink>
+            </div>
+            <div className="nav-active">
+              <NavLink
+                className={(navData) => (navData.isActive ? "nave-active" : "")}
+                to="/"
+              >
+                About
+              </NavLink>
+            </div>
+            <div className="nav-active">
+              <NavLink
+                className={(navData) => (navData.isActive ? "nave-active" : "")}
+                to="/"
+              >
+                Services
+              </NavLink>
+            </div>
+          </div>
+          <div className="Section2">
+            <img src={player} />
+          </div>
         </div>
-    );
-}
+      </MediaQuery>
+      <MediaQuery minResolution="2dppx">
+        <div className="logo-resp">
+          <Logo />
+        </div>
+        <div className="resp">
+          <div style={{width: "60%"}}>
+            <div>
+              <h1 className="title-resp">PING PONG GAME</h1>
+            </div>
+            <div className="paragresp">
+              <p style={{ textAlign: "justify", lineHeight: 1.6 }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+                corporis. Magni enim sit eos quia ad dicta beatae sint, esse
+                modi at unde asperiores dolor eaque laudantium, voluptates
+                provident suscipit.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="button-resp">
+          <div className="butt-des">SIGN IN</div>
+        </div>
+      </MediaQuery>
+    </div>
+  );
+};
 export default Home;
