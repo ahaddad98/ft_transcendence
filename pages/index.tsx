576b5649ae6f7de52b9397150632527f9de0f252
ahaddad98 <amine.haddadd@gmail.com>
2022-03-02 17:35:52 +0100

    landing page without responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,14 @@ export default function Home() {
     <Background>
       <Navbar />
       <Player />
-      <div>
+      <div className="title">
+      PING PONG GAME
+      </div>
+      <div className="parag">
+      Ping Pong est un jeu de sport créé par MarketJS. Prenez l'une des pagaies numériques et vivez une expérience passionnante de ping-pong. Dans ce jeu de sport simple mais stimulant, vous devez frapper la balle du côté de la table de votre adversaire. Le premier joueur à atteindre 10 gagne le match ! Soyez donc rapide et confondez votre adversaire avec des tirs difficiles. Vous pouvez même contrôler la vitesse de vos coups en faisant attention à l'intensité de votre coup. Allez-y et battez les adversaires intelligents de l'IA de ce jeu !
+      </div>
+      <div className="button">
+        SIGN IN
       </div>
     </Background>
   )
