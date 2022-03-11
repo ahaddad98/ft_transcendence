3b4d4d4fdbb313c0303d75e3f22ebf2ac113e951
ahaddad98 <amine.haddadd@gmail.com>
2022-03-11 18:57:06 +0100

    ss

--- front-end/pages/loginSuccess.tsx
+++ front-end/pages/loginSuccess.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HomeNavbar from "../components/HomeNavbar"
 import Head from 'next/head'
 import Link from 'next/link'
@@ -12,24 +12,39 @@ import SignIN from '../components/Signin'
 import { useState } from 'react'
 
 const loginSuccess = () => {
-    const router = useRouter();
-    console.log(router.query);
+    const [token, setToken] = useState('');
+    const [load, setLoad] = useState(true);
+    // const router = useRo9uter();
+    // const {s} = router.query;
+    const { query } = useRouter();
+    useEffect(() => {
+        // handleSubmit;
+        if (query.token)
+        {
+            console.log(query.token);
+            // setLoad(false);
+            setToken(query.token.toString());
+        }
+    }, [query])
     const handleSubmit = async (e) => {
+        console.log('im in handle');
         e.preventDefault();
-        
+        setLoad(true);
         try {
-            console.log('hhasiygha');
-            
             const response = await axios.get('http://localhost:3001/users/me', {headers: 
             { Authorization: `Bearer ${router.query}` }});
-            console.log(response);
+            // console.log(response);
         } catch (error) {
-          console.log(error);
+        //   console.log(error);
         }
+
     }
     
-    
     return (
+        <div>
+            {
+                1 ?
+               <div>hello</div> :
     <div>
         <form onSubmit={handleSubmit} encType="multipart/form-data">
         <label >Select image:</label>
@@ -39,6 +54,8 @@ const loginSuccess = () => {
         <input type="submit" value="Login"/>
         </form>
     </div>
+    }
+        </div>
     );
 }
 
