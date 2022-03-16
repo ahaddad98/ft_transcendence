33b7ce394db0202f0ecb7fe616bfba2d799f338a
ahaddad98 <amine.haddadd@gmail.com>
2022-03-16 22:11:59 +0100

    change navbar

--- front-end/components/HomeNavbar.tsx
+++ front-end/components/HomeNavbar.tsx
@@ -8,19 +8,73 @@ import Menu from '@mui/material/Menu';
 import Link from 'next/link';
 import { CssBaseline } from '@mui/material';
 import MediaQuery from "react-responsive";
-
+import avat from "../public/avatar.svg"
 const HomeNavbar = (props) => {
-    return (
-        <div className="abnf">
-            <ul className="menu-bar">
-                <li> Watch Now</li>
-	            <li> Movies</li>
-	            <li> TV Shows</li>
-	            <li> Sports</li>
-	            <li> Kids</li>
-	            <li> Library</li>
+        return (
+            <div>
+
+    <div className="flex flex-wrap bg-orange-400">
+      <section className=" relative mx-auto">
+        <nav className="flex justify-between bg-orange-400 text-white w-screen">
+          <div className="px-5 xl:px-12 py-6 flex w-full items-center">
+            <a className="text-3xl font-bold font-heading" href="#">
+              HOME
+            </a>
+            <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+              <li><a className="hover:text-gray-200" href="#">Watch Now</a></li>
+              <li><a className="hover:text-gray-200" href="#">PlayGame</a></li>
+              <li><a className="hover:text-gray-200" href="#">Friends</a></li>
             </ul>
+            <div className="xl:flex items-center space-x-5 items-between">
+              <a className="flex items-center hover:text-gray-200" href="#">
+                <span className="flex absolute -mt-5 ml-4">
+                  <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
+                    <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
+                    </span>
+                  </span>
+                  <img id="" src="/message.svg" className="h-7"/>
+              </a>
+              <a className="flex items-center hover:text-gray-200" href="#">
+                <span className="flex absolute -mt-5 ml-4">
+                  <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
+                    <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
+                    </span>
+                  </span>
+                  <img id="" src="/notif.svg" className="h-7"/>
+              </a>
+              <a className="flex items-center hover:text-gray-200" href="#">
+                <img id="" src="/avatar.svg" className="h-7"/>
+              </a>
+            </div>
+          </div>
+          <a className="xl:hidden flex mr-6 items-center" href="#">
+            <svg className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+            </svg>
+            <span className="flex absolute -mt-5 ml-4">
+              <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
+              </span>
+            </span>
+          </a>
+          <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+          </a>
+        </nav>
+        
+      </section>
+    </div>
+    <div className="absolute bottom-0 right-0 mb-4 mr-4 z-10">
+        <div>
+            <a title="Follow me on twitter" href="https://www.twitter.com/asad_codes" target="_blank" className="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
+                <img className="object-cover object-center w-full h-full rounded-full" src="https://www.imore.com/sites/imore.com/files/styles/large/public/field/image/2019/12/twitter-logo.jpg"/>
+            </a>
+        </div>
+    </div>
         </div>
   );
 };
+
 export default HomeNavbar;
