6a59e2d44916e93f8348f89be8f846c35806b2f3
ahaddad98 <amine.haddadd@gmail.com>
2022-04-01 01:51:05 +0000

    add channel chat && add message components

--- front-end/components/ChannelChat.tsx
+++ front-end/components/ChannelChat.tsx
@@ -1,17 +1,27 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Messagemap from "./Messagemap";
 
 const ChannelChat = (props) => {
     console.log(props.mychannel.conversation?.id);
-    const [conversation, setConversation] = useState({});
-    const fetchconsversation= async (id) => {
+    const [conversation, setConversation] = useState();
+    const fetchconsversation= async () => {
       const response = await axios.get(`http://localhost:3001/conversations/${props.mychannel.conversation?.id}/messages`, {
         headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
       });
       return response;
     };
     useEffect(() => {
-        fetchconsversation(props.mychannels)
+        fetchconsversation()
+        .then((res)=>{
+            if (res.data)
+            {
+                setConversation(res.data)
+            }
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
       }, [props.mychannels]);
     const [msg, setMsg] = useState('')
     const sendmsg = async (e) => {
@@ -37,27 +47,10 @@ const ChannelChat = (props) => {
           <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4">
             <div className="flex flex-col h-full overflow-x-auto mb-4">
               <div className="flex flex-col h-full">
-                <div className="grid grid-cols-12 gap-y-2">
-                  <div className="col-start-1 col-end-8 p-3 rounded-lg">
-                    <div className="flex flex-row items-center">
-                      <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                        A
-                      </div>
-                      <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                        <div>Hey How are you today?</div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-start-6 col-end-13 p-3 rounded-lg">
-                    <div className="flex items-center justify-start flex-row-reverse">
-                      <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                        A
-                      </div>
-                      <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                        <div>I'm ok what about you?</div>
-                      </div>
-                    </div>
-                  </div>
+                  <div className="grid grid-cols-12 gap-y-2">
+                  { conversation && 
+                        <Messagemap conv={conversation}/>
+                }
                 </div>
               </div>
             </div>
