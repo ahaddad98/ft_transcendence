7c08981d15d099bf1671559cbd884e63668f9bf8
ahaddad98 <amine.haddadd@gmail.com>
2022-04-02 16:47:56 +0000

    message error fixed

--- front-end/components/ChannelChat.tsx
+++ front-end/components/ChannelChat.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import Messagemap from "./Messagemap";
 
 const ChannelChat = (props) => {
-  console.log(props.mychannel.conversation?.id);
   const [conversation, setConversation] = useState();
   const fetchconsversation = async () => {
     const response = await axios.get(
@@ -81,23 +80,6 @@ const ChannelChat = (props) => {
                   className="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
                   onChange={(e) => setMsg(e.target.value)}
                 />
-                <button className="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
-                  <input type="submit" />
-                  <svg
-                    className="w-6 h-6"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    ></path>
-                  </svg>
-                </button>
               </form>
             </div>
           </div>
--- front-end/components/Messagemap.tsx
+++ front-end/components/Messagemap.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 const Messagemap = (props) => {
-  console.log(props.conv);
-
   return (
     <>
       {props.conv.map((stat, key) => {
@@ -12,7 +10,7 @@ const Messagemap = (props) => {
               <div className="col-start-1 col-end-8 p-3 rounded-lg">
                 <div className="flex flex-row items-center">
                   <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                    A
+                    <img src={stat.sender.avatar} />
                   </div>
                   <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
                     <div>{stat.content}</div>
@@ -24,7 +22,7 @@ const Messagemap = (props) => {
               <div className="col-start-6 col-end-13 p-3 rounded-lg">
                 <div className="flex items-center justify-start flex-row-reverse">
                   <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                    A
+                  <img src={stat.sender.avatar} />
                   </div>
                   <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
                     <div>{stat.content}</div>
