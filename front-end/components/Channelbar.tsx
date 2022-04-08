03e1a4d570ce4eaf5e1d155628288889aaf41b5b
ahaddad98 <amine.haddadd@gmail.com>
2022-04-08 17:28:30 +0000

    error when click users fixed

--- front-end/components/Channelbar.tsx
+++ front-end/components/Channelbar.tsx
@@ -22,6 +22,8 @@ const ChannelBar = (props) => {
   const [Clickmute, setClickmute] = useState(false);
   const [isban1, setisban1] = useState(false);
   const [ismute1, setIsmute1] = useState(false);
+  const [isbanadmin, setisbanadmin] = useState(false);
+  const [ismuteadmin, setIsmuteadmin] = useState(false);
   let isban = false;
   let ismute = false;
 
@@ -167,8 +169,7 @@ const ChannelBar = (props) => {
                       setUserid(stat.id);
                       if (stat.stat === "ban") setisban1(true);
                       if (stat.stat === "mute") setIsmute1(true);
-                      if (stat.stat == null)
-                      {
+                      if (stat.stat == null) {
                         setisban1(false);
                         setIsmute1(false);
                       }
@@ -666,120 +667,126 @@ const ChannelBar = (props) => {
           <div className="flex flex-col space-y-1 mt-4 -mx-2 h-48 overflow-y-auto">
             {props.mychannelusers.admins.map((stat, key) => {
               return (
-                <button
-                  className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2"
-                  key={key}
-                  onClick={() => {
-                    setClickadmin(!Clickadmin);
-                    setUserid(stat.id);
-                  }}
-                >
-                  <div className="flex items-center justify-center h-8 w-8 bg-indigo-200 rounded-full">
-                    <img src={stat.avatar} />
-                  </div>
-                  <div className="ml-2 text-sm font-semibold">
-                    {stat.username}
-                  </div>
-                </button>
+                <>
+                  <button
+                    className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2"
+                    key={key}
+                    onClick={() => {
+                      setClickadmin(!Clickadmin);
+                      setUserid(stat.id);
+                    }}
+                  >
+                    <div className="flex items-center justify-center h-8 w-8 bg-indigo-200 rounded-full">
+                      <img src={stat.avatar} />
+                    </div>
+                    <div className="ml-2 text-sm font-semibold">
+                      {stat.username}
+                    </div>
+                  </button>
+                  {Clickadmin && imowner && (
+                    <Modal
+                      size="lg"
+                      active={Clickadmin}
+                      toggler={() => setClickadmin(false)}
+                    >
+                      <div className="w-full md:w-auto dark:bg-gray-800 flex flex-col justify-center items-center bg-white  md:px-24 xl:py-4 xl:px-18">
+                        <div
+                          role="button"
+                          aria-label="MAIN BUTTON"
+                          className="inline-flex mt-2 xs:mt-0 bg-orange-500	w-32"
+                        >
+                          <button
+                            className="text-sm text-indigo-50 transition duration-150 hover:bg-orange-400 font-semibold py-2 px-4 rounded-r"
+                            onClick={(e) => {
+                              hundelkickuser(e);
+                            }}
+                          >
+                            Kick
+                          </button>
+                        </div>
+                        <div
+                          role="button"
+                          aria-label="MAIN BUTTON"
+                          className="inline-flex mt-2 xs:mt-0 bg-orange-500	w-32"
+                        >
+                          <button
+                            className="text-sm text-indigo-50 transition duration-150 hover:bg-orange-400 font-semibold py-2 px-4 rounded-r"
+                            onClick={() => {
+                              setClickban(!Clickban);
+                            }}
+                          >
+                            Ban
+                          </button>
+                        </div>
+                        {Clickban && (
+                          <form onSubmit={(e) => hundelsubmittime(e)}>
+                            <div className="w-32">
+                              <div>
+                                <input
+                                  type="text"
+                                  id="time"
+                                  placeholder="Time"
+                                  className="bg-indigo-50 px-2 py-2 outline-none rounded-md w-32 mt-1"
+                                  onChange={(e) =>
+                                    setSelectedtime(e.target.value)
+                                  }
+                                />
+                              </div>
+                            </div>
+                          </form>
+                        )}
+                        <div
+                          role="button"
+                          aria-label="MAIN BUTTON"
+                          className="inline-flex mt-2 xs:mt-0 bg-orange-500	w-32"
+                        >
+                          <button
+                            className="text-sm text-indigo-50 transition duration-150 hover:bg-orange-400 font-semibold py-2 px-4 rounded-r"
+                            onClick={() => {
+                              setClickmute(!Clickmute);
+                            }}
+                          >
+                            Mute
+                          </button>
+                        </div>
+                        {Clickmute && (
+                          <form onSubmit={(e) => hundelsubmittimemute(e)}>
+                            <div className="w-32">
+                              <div>
+                                <input
+                                  type="text"
+                                  id="time"
+                                  placeholder="Time"
+                                  className="bg-indigo-50 px-2 py-2 outline-none rounded-md w-32 mt-1"
+                                  onChange={(e) =>
+                                    setSelectedtime(e.target.value)
+                                  }
+                                />
+                              </div>
+                            </div>
+                          </form>
+                        )}
+                        <div
+                          role="button"
+                          aria-label="MAIN BUTTON"
+                          className="inline-flex mt-2 xs:mt-0 bg-orange-500	w-32"
+                        >
+                          <button
+                            className="text-sm text-indigo-50 transition duration-150 hover:bg-orange-400 font-semibold py-2 px-4 rounded-r"
+                            onClick={(e) => {
+                              hundelfireadmin(e);
+                            }}
+                          >
+                            Fire the admin
+                          </button>
+                        </div>
+                      </div>
+                    </Modal>
+                  )}
+                </>
               );
             })}
           </div>
-          {Clickadmin && imowner && (
-            <Modal
-              size="lg"
-              active={Clickadmin}
-              toggler={() => setClickadmin(false)}
-            >
-              <div className="w-full md:w-auto dark:bg-gray-800 flex flex-col justify-center items-center bg-white  md:px-24 xl:py-4 xl:px-18">
-                <div
-                  role="button"
-                  aria-label="MAIN BUTTON"
-                  className="inline-flex mt-2 xs:mt-0 bg-orange-500	w-32"
-                >
-                  <button
-                    className="text-sm text-indigo-50 transition duration-150 hover:bg-orange-400 font-semibold py-2 px-4 rounded-r"
-                    onClick={(e) => {
-                      hundelkickuser(e);
-                    }}
-                  >
-                    Kick
-                  </button>
-                </div>
-                <div
-                  role="button"
-                  aria-label="MAIN BUTTON"
-                  className="inline-flex mt-2 xs:mt-0 bg-orange-500	w-32"
-                >
-                  <button
-                    className="text-sm text-indigo-50 transition duration-150 hover:bg-orange-400 font-semibold py-2 px-4 rounded-r"
-                    onClick={() => {
-                      setClickban(!Clickban);
-                    }}
-                  >
-                    Ban
-                  </button>
-                </div>
-                {Clickban && (
-                  <form onSubmit={(e) => hundelsubmittime(e)}>
-                    <div className="w-32">
-                      <div>
-                        <input
-                          type="text"
-                          id="time"
-                          placeholder="Time"
-                          className="bg-indigo-50 px-2 py-2 outline-none rounded-md w-32 mt-1"
-                          onChange={(e) => setSelectedtime(e.target.value)}
-                        />
-                      </div>
-                    </div>
-                  </form>
-                )}
-                <div
-                  role="button"
-                  aria-label="MAIN BUTTON"
-                  className="inline-flex mt-2 xs:mt-0 bg-orange-500	w-32"
-                >
-                  <button
-                    className="text-sm text-indigo-50 transition duration-150 hover:bg-orange-400 font-semibold py-2 px-4 rounded-r"
-                    onClick={() => {
-                      setClickmute(!Clickmute);
-                    }}
-                  >
-                    Mute
-                  </button>
-                </div>
-                {Clickmute && (
-                  <form onSubmit={(e) => hundelsubmittimemute(e)}>
-                    <div className="w-32">
-                      <div>
-                        <input
-                          type="text"
-                          id="time"
-                          placeholder="Time"
-                          className="bg-indigo-50 px-2 py-2 outline-none rounded-md w-32 mt-1"
-                          onChange={(e) => setSelectedtime(e.target.value)}
-                        />
-                      </div>
-                    </div>
-                  </form>
-                )}
-                <div
-                  role="button"
-                  aria-label="MAIN BUTTON"
-                  className="inline-flex mt-2 xs:mt-0 bg-orange-500	w-32"
-                >
-                  <button
-                    className="text-sm text-indigo-50 transition duration-150 hover:bg-orange-400 font-semibold py-2 px-4 rounded-r"
-                    onClick={(e) => {
-                      hundelfireadmin(e);
-                    }}
-                  >
-                    Fire the admin
-                  </button>
-                </div>
-              </div>
-            </Modal>
-          )}
         </div>
         <div className="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between">
           <div className="inline-flex mt-2 xs:mt-0">
